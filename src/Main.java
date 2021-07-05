806c66fb0897f8faf642dd62642fab69d1c25fc6
Lera Kazarina <lera.kazarina21@gmail.com>
2021-07-05 15:41:43 +0700

    Added Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import javax.swing.text.html.parser.Parser;
+import java.awt.*;
 import java.io.*;
+import java.text.ParsePosition;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -10,15 +15,16 @@ public class Main {
         GameProgress gameProgress1 = new GameProgress(3, 10,32,2);
         GameProgress gameProgress2 = new GameProgress(5,11,32,4);
 
+        saveGame("C://Workspace//13.1.2//myFile.dat", gameProgress);
+        saveGame("C://Workspace//13.1.2//myFile2.dat", gameProgress1);
+        saveGame("C://Workspace//13.1.2//myFile3.dat", gameProgress2);
 
-        saveGame("C://Workspace//13.1.2//myFile1", gameProgress);
-        saveGame("C://Workspace//13.1.2//myFile2", gameProgress1);
-        saveGame("C://Workspace//13.1.2//myFile3", gameProgress2);
+        ArrayList<String> list= new ArrayList();
+        list.add("C://Workspace//13.1.2//myFile.dat");
+        list.add("C://Workspace//13.1.2//myFile2.dat");
+        list.add("C://Workspace//13.1.2//myFile3.dat");
 
-        zipFiles("C://Workspace//13.1.2//zip_output.zip", "C://Workspace//13.1.2//myFile1");
-        zipFiles("C://Workspace//13.1.2//zip_output.zip", "C://Workspace//13.1.2//myFile2");
-        zipFiles("C://Workspace//13.1.2//zip_output.zip", "C://Workspace//13.1.2//myFile3");
-
+        zipFiles("C://Workspace//13.1.2//zip_output.zip", list);
         }
 
     private static void saveGame(String file, GameProgress gameProgress) {
@@ -31,15 +37,16 @@ public class Main {
         }
     }
 
-    private static void zipFiles(String archive, String file) {
-        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("zip_output.zip"));
-            FileInputStream fis = new FileInputStream(file)) {
-            ZipEntry entry = new ZipEntry("myFile.txt");
-            zout.putNextEntry(entry);
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            zout.write(buffer);
-            zout.closeEntry();
+    private static void zipFiles(String archive, ArrayList<String> arrayList) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(archive))) {
+            try (FileInputStream fis = new FileInputStream(String.valueOf(arrayList))) {
+                ZipEntry entry = new ZipEntry("packed_notes.txt");
+                zout.putNextEntry(entry);
+                byte[] buffer = new byte[fis.available()];
+                fis.read(buffer);
+                zout.write(buffer);
+                zout.closeEntry();
+            }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
